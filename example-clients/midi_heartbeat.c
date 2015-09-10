0b7c6ae66ad9aa0fd3a369281b41f2050f0eba73
Thomas Brand <tom@trellis.ch>
2015-09-10 04:42:53 +0200

    add methods to send different kinds of midi messages. midi channel 0-15.

--- example-clients/midi_heartbeat.c
+++ example-clients/midi_heartbeat.c
@@ -15,6 +15,14 @@ int main(int argc, char *argv[]);
 static void signal_handler(int sig);
 static void shutdown_callback(void *arg);
 static void jack_error(const char* err);
+static void send_midi_note(int channel,int pitch,int velocity,int ison,int pos);
+static void send_midi_controller(int channel,int controller_number,int controller_value,int pos);
+static void send_midi_aftertouch(int channel,int pitch,int touch,int pos);
+static void send_midi_pitchbend(int channel,int lsb,int msb,int pos);
+static void send_midi_program_change(int channel,int program,int pos);
+static void send_midi_channel_pressure(int channel,int pressure,int pos);
+static void send_midi_realtime(int number,int pos);
+
 static int process(jack_nframes_t nframes, void *arg);
 
 typedef jack_default_audio_sample_t sample_t;
@@ -184,7 +192,7 @@ static void jack_error(const char* err)
 }
 
 //===================================================================
-static void send_midi(int channel,int pitch,int ison,int velocity,int pos)
+static void send_midi_note(int channel,int pitch,int velocity,int ison,int pos)
 {
 	jack_midi_data_t *buffer;
 	buffer = jack_midi_event_reserve(buffer_out_midi, pos, 3);
@@ -198,6 +206,62 @@ static void send_midi(int channel,int pitch,int ison,int velocity,int pos)
 	{
 		buffer[0] = (0x80 | channel);       /* note off */
 	}
+}
+
+//===================================================================
+static void send_midi_controller(int channel,int controller_number,int controller_value,int pos)
+{
+	jack_midi_data_t *buffer;
+	buffer = jack_midi_event_reserve(buffer_out_midi, pos, 3);
+		buffer[2] = controller_value,
+		buffer[1] = controller_number;
+		buffer[0] = (0xB0 | channel);
+}
+
+//===================================================================
+static void send_midi_aftertouch(int channel,int pitch,int touch,int pos)
+{
+	jack_midi_data_t *buffer;
+	buffer = jack_midi_event_reserve(buffer_out_midi, pos, 3);
+		buffer[2] = touch,
+		buffer[1] = pitch;
+		buffer[0] = (0xA0 | channel);
+}
+
+//===================================================================
+static void send_midi_pitchbend(int channel,int lsb,int msb,int pos)
+{
+	jack_midi_data_t *buffer;
+	buffer = jack_midi_event_reserve(buffer_out_midi, pos, 3);
+		buffer[2] = msb,
+		buffer[1] = lsb;
+		buffer[0] = (0xE0 | channel);
+}
+
+//===================================================================
+static void send_midi_program_change(int channel,int program,int pos)
+{
+	jack_midi_data_t *buffer;
+	buffer = jack_midi_event_reserve(buffer_out_midi, pos, 2);
+		buffer[1] = program;
+		buffer[0] = (0xC0 | channel);
+}
+
+//===================================================================
+static void send_midi_channel_pressure(int channel,int pressure,int pos)
+{
+	jack_midi_data_t *buffer;
+	buffer = jack_midi_event_reserve(buffer_out_midi, pos, 2);
+		buffer[1] = pressure;
+		buffer[0] = (0xD0 | channel);
+}
+
+//===================================================================
+static void send_midi_realtime(int number,int pos)
+{
+	jack_midi_data_t *buffer;
+	buffer = jack_midi_event_reserve(buffer_out_midi, pos, 1);
+		buffer[0] = (0xF0 | number);
 }
 
 //=============================================================================
@@ -225,7 +289,41 @@ static int process(jack_nframes_t nframes, void *arg)
 		if(frame_counter % frames_between_on_off == 0)
 		{
 			fprintf(stderr,".");
-			send_midi(channel_counter,pitch_counter,is_on,velocity_counter,k);
+			send_midi_note			(channel_counter,pitch_counter, velocity_counter,is_on,k);
+/*
+			send_midi_controller		(channel_counter,pitch_counter,	velocity_counter,k);
+			send_midi_aftertouch		(channel_counter,pitch_counter,	velocity_counter,k);
+			send_midi_pitchbend		(channel_counter,pitch_counter,	velocity_counter,k);
+			send_midi_program_change	(channel_counter,		velocity_counter,k);
+			send_midi_channel_pressure	(channel_counter,		velocity_counter,k);
+//			send_midi_realtime		(				velocity_counter,k);
+*/
+
+/*
+jack_midi_dump output
+...
+   1: 90 22 6f note on  (channel  0): pitch  34, velocity 111
+   2: b0 22 6f control change (channel  0): controller  34, value 111
+   3: a0 22 6f 		#after touch
+   4: e0 22 6f 		#pitchbend
+   5: c0 6f		#program change
+   0: d0 6f		#channel pressure
+   0: 80 22 70 note off (channel  0): pitch  34, velocity 112
+   1: b0 22 70 control change (channel  0): controller  34, value 112
+   2: a0 22 70
+   3: e0 22 70
+   4: c0 70
+   0: d0 70
+   1: 90 22 71 note on  (channel  0): pitch  34, velocity 113
+   2: b0 22 71 control change (channel  0): controller  34, value 113
+   3: a0 22 71
+   4: e0 22 71
+   5: c0 71
+   0: d0 71
+...
+*/
+
+			//toggle
 			if(is_on)
 			{
 				o1[k]=0.8;
@@ -245,13 +343,13 @@ static int process(jack_nframes_t nframes, void *arg)
 				{
 					pitch_counter=0;
 					channel_counter++;
-					if(channel_counter>16)
+					if(channel_counter>15)
 					{
 						channel_counter=0;
 					}
 				}
 			}
-		}
+		}//end if frame matches output grid
 		frame_counter++;
 	}
 
